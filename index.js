01db81ae4d73f4655617f8563269321465a87869
yuyucode1125 <1440771511@qq.com>
2021-07-15 03:02:09 +0800

    'c3'

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
+import path from 'path'
 import pkg from '@prisma/client';
 const { PrismaClient } = pkg;
 const {post, user} = new PrismaClient()
 
 console.log(post)
+
+
+/** 添加服务器端的内容 **/
+
+const a = path.resolve("/...")
+console.log(a)
